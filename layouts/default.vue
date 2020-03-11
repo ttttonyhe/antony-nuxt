e5d8bd7f33ae3282ff5334534a2899a683f8e8e9
HelipengTony <he@holptech.com>
2020-03-11 11:02:01 +0800

    Chore: forgot what did I change

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,9 +23,9 @@
 
 <script>
 // PC & Mobile Navigation Menu Components
-import mobileNav from "~/components/headers/mobileNav";
-import computerNav from "~/components/headers/computerNav";
-import siteFooter from "~/components/footer";
+import mobileNav from '~/components/headers/mobileNav'
+import computerNav from '~/components/headers/computerNav'
+import siteFooter from '~/components/footer'
 
 // Import RemixIcon feature
 import 'remixicon/fonts/remixicon.css'
@@ -34,7 +34,7 @@ export default {
   data() {
     return {
       mobile: false
-    };
+    }
   },
   components: {
     mobileNav,
@@ -47,9 +47,21 @@ export default {
         lang: 'zh-cn'
       },
       meta: [
-        { hid: 'msvalidate.01', name: 'msvalidate.01', content: '4D922B4BDF9F7A9398A7ECCF7B9F0365' },
-        { hid: '360-site-verification', name: '360-site-verification', content: '5f2cc87b24f0b9eddfc0c3a750941bbc' },
-        { hid: 'google-site-verification', name: 'google-site-verification', content: '_Q8Sk4WSzRH3ZGwwe2J1V2WUTdQFP9Pg6PSKdUKQXP4' }
+        {
+          hid: 'msvalidate.01',
+          name: 'msvalidate.01',
+          content: '4D922B4BDF9F7A9398A7ECCF7B9F0365'
+        },
+        {
+          hid: '360-site-verification',
+          name: '360-site-verification',
+          content: '5f2cc87b24f0b9eddfc0c3a750941bbc'
+        },
+        {
+          hid: 'google-site-verification',
+          name: 'google-site-verification',
+          content: '_Q8Sk4WSzRH3ZGwwe2J1V2WUTdQFP9Pg6PSKdUKQXP4'
+        }
       ]
     }
   },
@@ -57,30 +69,32 @@ export default {
     // Detect Mobile or PC using UserAgent
     let flag = navigator.userAgent.match(
       /(Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-    );
+    )
     if (flag) {
-      this.mobile = true;
+      this.mobile = true
     } else {
-      this.mobile = false;
+      this.mobile = false
     }
 
-    // Baidu analysis
-    var _hmt = _hmt || [];
-    (function() {
-      var hm = document.createElement("script");
-      hm.src = "https://hm.baidu.com/hm.js?20265c137ab04d39313561665f1ae7a1";
-      var s = document.getElementsByTagName("script")[0]; 
-      s.parentNode.insertBefore(hm, s);
-    })();
+    if (process.env.NODE_ENV === 'production') {
+      // Baidu analysis
+      var _hmt = _hmt || []
+      ;(function() {
+        var hm = document.createElement('script')
+        hm.src = 'https://hm.baidu.com/hm.js?20265c137ab04d39313561665f1ae7a1'
+        var s = document.getElementsByTagName('script')[0]
+        s.parentNode.insertBefore(hm, s)
+      })()
+    }
   }
-};
+}
 </script>
 
 <style lang="scss">
 [v-cloak] {
   display: none !important;
 }
-#nprogress .bar{
+#nprogress .bar {
   background: #3797fe !important;
   z-index: 9999;
 }
