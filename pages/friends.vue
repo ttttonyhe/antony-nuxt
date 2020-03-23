a8ecdca4740fbb987c8d0a9e6950bd3c1c2ed5c6
HelipengTony <he@holptech.com>
2020-03-23 16:03:05 +0800

    Improve: Fisher Yates shuffle

--- pages/friends.vue
+++ pages/friends.vue
@@ -81,11 +81,17 @@ export default {
   async asyncData(context) {
     let res = await Promise.all([
       context.$axios
-        .get('https://blog.ouorz.com/wp-json/wp/v2/posts?&per_page=100&categories=2&orderby=title&order=asc')
+        .get('https://blog.ouorz.com/wp-json/wp/v2/posts?&per_page=100&categories=2')
         .then(response => {
           return response.data
         })
     ])
+
+    // Fisher-Yates Shuffle 洗牌随机排序
+    for (let i = res[0].length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let t = res[0][i]; res[0][i] = res[0][j]; res[0][j] = t;
+    }
 
     return {
       posts: res[0]
